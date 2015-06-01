b5cfbd91211313bd1508d065e4f7a22338c67a7e
yaokai1117 <yaokai1117@gmail.com>
2015-06-01 22:13:46 +0800

    mrutil

---
+++ mrutil.b
@@ -0,0 +1,29 @@
+implement MRUtil;
+
+include "mrutil.m";
+include "sys.m";
+sys : Sys;
+
+init()
+{
+	sys = load Sys Sys->PATH;
+}
+
+
+MapperTask.toString(mp : self ref MapperTask) : string
+{
+	ret := sys->sprint("id: %d, jobId: %d, status: %d, reducerAmount: %d, attempt: %d\n", mp.id, mp.jobId, mp.status, mp.reducerAmount, mp.attemptCount);
+	return ret;
+}
+
+ReducerTask.toString(rd : self ref ReducerTask) : string
+{
+	ret := sys->sprint("id: %d, jobId: %d, status: %d, mapperAmount: %d, attempt: %d\n", rd.id, rd.jobId, rd.status, rd.mapperAmount, rd.attemptCount);
+	return ret;
+}
+
+TaskTrackerInfo.toString(tt : self ref TaskTrackerInfo) : string
+{
+	ret := sys->sprint("addr: %s, port: %d, mapperTaskNum: %d, reducerTaskNum: %d", tt.addr, tt.port, tt.mapperTaskNum, tt.reducerTaskNum);
+	return ret;
+}
--- mrutil.m
+++ mrutil.m
@@ -4,12 +4,12 @@ MRUtil : module {
 	SUCCESS : con 0;
 	PENDING : con 1;
 	INIT 	: con 2;
-	FALIED 	: con -1;
+	FAILED 	: con -1;
 
 		
 	MapperTask : adt{
 		# common task properties 
-		taskId : int;
+		id : int;
 		jobId : int;
 		status : int;		
 		attemptCount : int;
@@ -17,14 +17,16 @@ MRUtil : module {
 		mrClassName : string;
 		outputDir : string;
 
+		toString : fn(mp : self ref MapperTask) : string;
+
 		# mapper
-		inputFileBlock : ref IOUtil->FileBlock;
+#		inputFileBlock : ref IOUtil->FileBlock;
 		reducerAmount : int;
 	};
 
 	ReducerTask : adt{
 		# common task properties 
-		taskId : int;
+		id : int;
 		jobId : int;
 		status : int;		
 		attemptCount : int;
@@ -32,6 +34,8 @@ MRUtil : module {
 		taskTrackerPort : int;
 		mrClassName : string;
 		outputDir : string;
+
+		toString : fn(rd : self ref ReducerTask) : string;
 
 		# reducer
 		outputFile : string;
@@ -42,10 +46,12 @@ MRUtil : module {
 	};
 
 	TaskTrackerInfo : adt {
-		address : string;
+		addr : string;
 		port : int;
 		mapperTaskNum : int;
 		reducerTaskNum : int;
+
+		toString : fn(tt : self ref TaskTrackerInfo) : string;
 	};
 
 
